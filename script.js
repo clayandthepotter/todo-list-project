33f400a0872887998b58a21c18674fd8891b3ab3
Clayton Prickett <71116858+clayandthepotter@users.noreply.github.com>
2023-08-11 00:02:32 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+//Call var form to interact with html id todo-form
 var form = document.getElementById('todo-form');
+
+//Call var input to interact with html id todo-input
+var input = document.getElementById('todo-input');
+
+//Call var list to interact with html id todo-list
+var list = document.getElementById('todo-list');
